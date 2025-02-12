266431185863534657c2fa069f5170c0cd5b2290
EllanorS <ellasplinter@outlook.com>
2025-02-11 23:48:02 -0600

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        //Game plays out here
+        //Initialize all the cards- implementation in cards.java
+        //
+    }
+}
